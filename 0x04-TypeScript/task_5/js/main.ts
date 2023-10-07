5e2f64e79d430d4732eaa8c7c83a2209f50f5b8b
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-10-07 05:41:27 +0100

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,14 @@
-h
+interface MajorCredits {
+    credits: number;
+    brand: string;
+}
+interface MinorCredits {
+    credits: number;
+    brand: string;
+}
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
+    return subject1.credits + subject2.credits;
+}
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+    return subject1.credits + subject2.credits;
+}
